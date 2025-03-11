0f9f4f5521bc7dd6bbdc25f711b69f5fd5a8f0e3
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-03-10 23:26:23 -0600

    Fix links de imagenes de Categoria

--- app/src/main/java/com/example/palonsofoodapp/models/Category.kt
+++ app/src/main/java/com/example/palonsofoodapp/models/Category.kt
@@ -12,18 +12,18 @@ val categoriesList = listOf(
     ),
     Category(
         name = "Comida china",
-        image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Ficono%2Fcomida-china_1400695&psig=AOvVaw22NWppp-5IjhV670TPXc8q&ust=1741750852967000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIj479mNgYwDFQAAAAAdAAAAABAE"
+        image = "https://cdn-icons-png.flaticon.com/512/1211/1211014.png"
     ),
     Category(
         name = "Comida italiana",
-        image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.es%2Ficono-gratis%2Fcomida-italiana_5930229&psig=AOvVaw3UFUM6l24Bp7YzlkCqqLpc&ust=1741750898698000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOiEhdmOgYwDFQAAAAAdAAAAABAE"
+        image = "https://cdn-icons-png.flaticon.com/512/2994/2994669.png"
     ),
     Category(
         name = "Comida mexicana",
-        image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Fvector-premium%2Filustracion-icono-vector-comida-mexicana-taco-objeto-comida-concepto-diseno-icono-amante-comida_32904909.htm&psig=AOvVaw177VfY6TefOn0vlb6CKym-&ust=1741751162057000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMi72euOgYwDFQAAAAAdAAAAABAI"
+        image = "https://cdn-icons-png.flaticon.com/512/3032/3032598.png"
     ),
     Category(
         name = "Postres",
-        image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.es%2Ficono-gratis%2Fpostres_7182828&psig=AOvVaw35GBCz50G03fcZVm7gt8SN&ust=1741751214543000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNDwsIWPgYwDFQAAAAAdAAAAABAE"
+        image = "https://cdn-icons-png.flaticon.com/512/5347/5347946.png"
     )
 )
