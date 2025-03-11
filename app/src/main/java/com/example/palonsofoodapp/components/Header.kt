aabf23aa9c7195dc58c1c825edeed6e758a2b2f3
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-03-10 22:25:07 -0600

    Header terminado y agregado customIcon de logout

--- app/src/main/java/com/example/palonsofoodapp/components/Header.kt
+++ app/src/main/java/com/example/palonsofoodapp/components/Header.kt
@@ -1,16 +1,38 @@
 package com.example.palonsofoodapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.palonsofoodapp.ui.theme.PAlonsoFoodAppTheme
+import com.example.palonsofoodapp.utils.CircleUser
+import com.example.palonsofoodapp.utils.Logout
 
 @Composable
 fun Header() {
     Row (
-
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(imageVector = CircleUser, contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Hola, Pablo", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        }
+        Image(imageVector = Logout, contentDescription = null)
     }
 }
 
