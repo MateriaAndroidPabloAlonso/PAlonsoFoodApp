89e5f3628d168ca8f385205e2cc88b0698a64778
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-03-10 23:07:15 -0600

    finalizacion CategoryItem

--- app/src/main/java/com/example/palonsofoodapp/components/CategoryItem.kt
+++ app/src/main/java/com/example/palonsofoodapp/components/CategoryItem.kt
@@ -1,21 +1,43 @@
 package com.example.palonsofoodapp.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.palonsofoodapp.models.Category
 import com.example.palonsofoodapp.models.categoriesList
 import com.example.palonsofoodapp.ui.theme.PAlonsoFoodAppTheme
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun CategoryItem(category: Category) {
-    Column {  }
+fun CategoryItem(categories: Category) {
+    Column (horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        GlideImage(
+            model = categories.image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(24.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Text(text = categories.name, fontSize = 14.sp)
+    }
 }
 
 @Preview
 @Composable
 fun CategoryItemPreview() {
     PAlonsoFoodAppTheme {
-        CategoryItem(category = categoriesList[0])
+        CategoryItem(categories = categoriesList[0])
     }
 }
